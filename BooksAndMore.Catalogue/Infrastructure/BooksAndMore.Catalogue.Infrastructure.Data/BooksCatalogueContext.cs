5d9858f98e8f137091bdfdac3ff1363920031ba3
tiggris <tiggris@gmail.com>
2018-01-07 14:34:50 +0100

    DbContext created

---
+++ BooksAndMore.Catalogue/Infrastructure/BooksAndMore.Catalogue.Infrastructure.Data/BooksCatalogueContext.cs
@@ -0,0 +1,20 @@
+﻿using BooksAndMore.Catalogue.Domain.Model.Authors;
+using BooksAndMore.Catalogue.Domain.Model.Books;
+using BooksAndMore.Catalogue.Domain.Model.Publishers;
+using System.Data.Entity;
+
+namespace BooksAndMore.Catalogue.Infrastructure.Data
+{
+    public class BooksCatalogueContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+        public BooksCatalogueContext(string nameOrConnectionString) :
+            base(nameOrConnectionString)
+        {
+            
+        }
+    }
+}
